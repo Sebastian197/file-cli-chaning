68f71eccbf47538ade5965f39c13d0712e4644ee
Sebastián Moreno <morenosebastian117@gmail.com>
2020-12-03 12:21:52 +0000

    Primera versión del generador de ficheros añadido correctamente

---
+++ README.md
@@ -0,0 +1,43 @@
+# Creador de ficheros mediante CLI
+
+En este package puedes crear ficheros introduciendo el nombre y con extensiones de tipo: ['.js', '.ts', '.rb', '.php', '.css', '.scss', '.json', '.html', '.java', '.kt', '.gitignore', '.npmignore', '.md'].
+
+## Instrucciones
+
+Para un correcto uso, hay que seguir estas instrucciones.
+
+### Instalación
+
+- GLOBAL
+
+```
+npm install -g file-cli-creator-chaning
+```
+
+- LOCAL
+
+```
+npm install file-cli-creator-chaning
+```
+
+### Para usarlo
+
+- GLOBAL
+
+Ejecutamos \*\*file-cli-creator-chaning
+
+- LOCAL
+
+Añadir en el package.json
+
+```json
+"scripts": {
+    "file-cli-creator-chaning": "./node_modules/.bin/file-cli-creator-chaning"
+},
+```
+
+Ejecutamos con
+
+```
+npm run file-cli-creator-chaning
+```
---
+++ index.js
@@ -0,0 +1,89 @@
+#!/user/bin/env node
+const shelljs = require('shelljs');
+const chalk = require('chalk');
+const figlet = require('figlet');
+const inquirer = require('inquirer');
+
+/**
+ * Muestra un mensaje por consola personalizado usando la librería chalk
+ * @param {string} filepath Ruta del fichero
+ */
+const fileOk = (filepath) => {
+    console.log(
+        chalk.white.bgGreen.bold(
+            '¡Muy bien! Fichero creado correctamente en el directorio '
+            + chalk.gray.italic(
+                `${filepath}`
+            )
+        )
+    );
+};
+
+/**
+ * Crea el fichero y retorna la ruta donde fue creado
+ * @example
+ * (/Volumes/file-cli-chaning/index.js:68:22)
+ * @param {string} nameFile Nombre del fichero
+ * @param {string} extension Tipo de extensión
+ * @returns {string} Nombre de la ruta
+ */
+const createFile = (nameFile, extension) => {
+    const pathFile = `${process.cwd()}/${nameFile}.${extension}`;
+    shelljs.touch(pathFile);
+    return pathFile;
+};
+
+/**
+ * Función que hace las preguntas para crear el ficheo
+ */
+const askQuestions = () => {
+    const asks = [
+        {
+            name: 'FILE',
+            type: 'input',
+            message: '¿Como se va a llamar tu fichero? (sin la extensión)'
+        },
+        {
+            name: 'EXTENSION',
+            type: 'list',
+            message: '¿Que extensión tiene tu fichero?',
+            choices: ['.js', '.ts', '.rb', '.php', '.css', '.scss', '.json', '.html', '.java', '.kt', '.gitignore', '.npmignore', '.md'],
+            filter: function (val) {
+                return val.split('.')[1];
+            }
+        },
+    ];
+    return inquirer.prompt(asks);
+};
+
+/**
+ * Muestra la  información de la librería en la cabecera
+ */
+const init = () => {
+    console.log(
+        chalk.green(
+            figlet.textSync('Crear ficheros', {
+                font: 'Big',
+                horizontalLayout: 'default',
+                verticalLayout: 'default'
+            })
+        )
+    )
+};
+
+/**
+ * Inicializador del programa
+ */
+const run = async () => {
+    // Mostrar la información de la librería en la cabecera, el título con figlet
+    init();
+    // Preguntas necesarias para crear el fichero, el nombre y la extención
+    const asks = await askQuestions();
+    const { FILE, EXTENSION } = asks;
+    // Crear el fichero
+    const pathFile = createFile(FILE, EXTENSION);
+    // Añadir mensaje que el fichero se ha creado correctamente
+    fileOk(pathFile);
+};
+
+run();
